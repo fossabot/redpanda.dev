21890348bc79a36dfd45174943c184d324d0be37
김용건 <jonathan@lsware.com>
2019-03-27 17:20:53 +0900

    chore: add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,23 @@
 
 module.exports = {
   mode: 'spa',
-  srcDir: 'src'
+  srcDir: 'src',
+  head: {
+    title: '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' }
+    ],
+    script: [
+      { crossorigin: 'anonymous', href: 'https://polyfill.io/v3/polyfill.min.js' }
+    ]
+  },
+  modules: [
+    '@nuxtjs/vuetify'
+  ],
+  vuetify: {
+    materialIcons: true,
+    css: true,
+    treeShake: true
+  }
 }
---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-app(:dark="darkMode")#top
+    v-content
+      v-container(fluid)
+        v-layout(align-space-around justify-center column fill-height)
+          nuxt
+</template>
+
+<script>
+export default {
+  name: 'RedpandaDev',
+  data: () => ({
+    darkMode: true
+  })
+}
+</script>
